ac7c9395cc487a1177bbceaae574edd8c6534679
ChemistryNarwhal <55032422+ChemistryNarwhal@users.noreply.github.com>
2024-11-29 12:34:23 -0500

    Update indext.py

--- indext.py
+++ indext.py
@@ -1,10 +1,13 @@
-pip install opencv
-
 import cv2
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 image = cv2.imread('image.jpg')
+
+if image is None:
+    print("Error: Could not load image.")
+    exit()
+
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
@@ -15,4 +18,3 @@ for (x, y, w, h) in faces:
 cv2.imshow('Detected Faces', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
