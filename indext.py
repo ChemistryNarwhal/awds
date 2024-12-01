84f1f610d8ee973e3b6cac13758ddcd5519151fa
ChemistryNarwhal <55032422+ChemistryNarwhal@users.noreply.github.com>
2024-12-01 16:24:47 -0500

    Update indext.py

--- indext.py
+++ indext.py
@@ -1,19 +1,9 @@
-import cv2
+pip install tensorflow tensorflow-gpu opencv matplotlib
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+import tensorflow as tf
+import os #nagation though file struters on all operating sistems 
 
-image = cv2.imread('image.jpg')
-
-if image is None:
-    print("Error: Could not load image.")
-    exit()
-
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-cv2.imshow('Detected Faces', image)
-cv2.waitKey(0)
+#stop from using all your memory
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    ft.config.experimental.set_memory_growth(gpu, True)
